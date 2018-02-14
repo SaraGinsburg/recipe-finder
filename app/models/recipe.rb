e22e6f1dade9423f15342abacd7b697ac9bf5e06
Luisa Scavo <luisascavo@gmail.com>
2018-02-14 08:27:47 -0500

    add content to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+  
   validates_presence_of :name
+
+  accepts_nested_attributes_for :ingredients
+  def self.most_recently_updated
+    order('updated_at desc').first
+  end
 end
