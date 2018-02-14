648ed77cef4816b98d9da1af70f114991689b8ab
Luisa Scavo <luisascavo@gmail.com>
2018-02-14 09:13:02 -0500

    write ingredients_attributes=

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,18 @@ class Recipe < ApplicationRecord
   
   validates_presence_of :name
 
-  accepts_nested_attributes_for :ingredients
+  # accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients_attributes)
+    ingredients_attributes.values.each do |ingredients_attribute|
+      # raise ingredients_attribute.inspect
+      if ingredients_attribute["name"] != ''
+        ingredient = Ingredient.find_or_create_by(name: ingredients_attribute["name"])
+        self.recipe_ingredients.build(ingredient: ingredient, quantity: ingredients_attribute["recipe_ingredients"]["quantity"])
+      end
+    end
+  end
+
   def self.most_recently_updated
     order('updated_at desc').first
   end
